f35d688098156e4e72c12a649034c7bc95c81232
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-27 09:09:41 -0500

    Created reusable components.

---
+++ src/subcomponents/Input.jsx
@@ -0,0 +1,12 @@
+const Input = ({ type, className, placeholder }) => {
+
+    return (
+        <input
+            type={type}
+            className={className}
+            placeholder={placeholder}
+        />
+    );
+};
+
+export default Input;
---
+++ src/subcomponents/TextArea.jsx
@@ -0,0 +1,13 @@
+const TextArea = ({ className, placeholder }) => {
+
+    return (
+        <textarea
+            className={className}
+            placeholder={placeholder}
+        >
+
+        </textarea>
+    );
+};
+
+export default TextArea;
