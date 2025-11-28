ab28d3ac2c04e030e7bf0837506c0f0e3f264d01
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-27 21:55:42 -0500

    Adding addition component pages .

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Home from "./components/Home";
 import PersonalDetails from "./components/PersonalDetails";
 import Links from "./components/Links";
 import Skills from "./components/Skills"
+import WorkExperience from "./components/WorkExperience";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Certifications from "./components/Certifications"
 
 function App() {
 
@@ -12,6 +16,10 @@ function App() {
       <Route path="/personal-details" element={<PersonalDetails />} />
       <Route path="/links" element={<Links />} />
       <Route path="/skills" element={<Skills />} />
+      <Route path="work-experience" element={<WorkExperience />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="education" element={<Education />} />
+      <Route path="certifications" element={<Certifications />} />
     </Routes>
   );
 };
