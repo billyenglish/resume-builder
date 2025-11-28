8557d67b9507bb46248daba15634288e33c58b01
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-27 22:02:26 -0500

    Creating the initial layout .

---
+++ src/components/Certifications.jsx
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Certifications = () => {
+
+    return (
+        <>
+            <Nav />
+            <section className="min-h-screen flex flex-col justify-center items-center gap-12">
+                    Certifications
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default Certifications;
