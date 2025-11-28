93aa555117116eb8ff06bd6b220e22b7686a8a8b
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-27 22:15:29 -0500

    Creating the initial layout .

---
+++ src/components/Education.jsx
@@ -0,0 +1,17 @@
+import Nav from "./Nav"
+import Footer from "./Footer";
+
+const Education = () => {
+
+    return (
+        <>
+            <Nav />
+            <section>
+                Education
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default Education;
