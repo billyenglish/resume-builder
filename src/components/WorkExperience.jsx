1cef5805493fa94a4740cd640647ba4e42ebe707
Demar English <demarenglish@Demars-MacBook-Pro.local>
2025-11-27 22:16:07 -0500

    Creating the initial layout .

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const WorkExperience = () => {
+
+    return (
+        <>
+            <Nav />
+            <section className="min-h-screen flex flex-col justify-center items-center gap-12">
+                W
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default WorkExperience;
